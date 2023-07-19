9cdf941e98601b372111dd7ca82640669b43454c
JackOBrien21 <101219751+JackOBrien21@users.noreply.github.com>
2023-07-19 14:36:25 -0400

    Added projects tab and imports to App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
-
+import Projects from "./pages/Projects"
 
 export default function App() {
+    useEffect( () => {
+        document.title = "Jack OBrien - Developer"
+    }, [])
+    
     return (
         <Router>
             <Navbar />
@@ -14,6 +18,7 @@ export default function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
+                <Route path="/projects" element={<Projects />} />
             </Routes>
         </Router>
     )
